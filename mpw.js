cfe3ec2965d4dc3647ff7aece43dba745f1401de
EmmaProg <emmanuelibanez607@gmail.com>
2024-05-23 22:36:26 -0300

    Add files via upload

---
+++ mpw.js
@@ -0,0 +1,9 @@
+console.error("hola");
+let numero;
+let numeroPar=2;
+const numeroImpar=1;
+numero=8;
+alert(numero);
+//numeroImpar=9;
+numero=prompt("Por favor, ingresa un número");
+alert(numero);
